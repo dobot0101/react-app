2dfc4eb14f40e26678945ec3e586284443742e29
dobot0101 <lhl890@naver.com>
2020-02-20 02:33:04 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.18d4e381ec30be05a096c8ac7f650d09.js"
+  "/react-app/precache-manifest.5ce10f1eee4c2dcba158ab63583f2668.js"
 );
 
 self.addEventListener('message', (event) => {
